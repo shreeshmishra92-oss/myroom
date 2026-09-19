ddc415f4a5f8ccef0b1e803dc16fdbf6da2dd839
unknown <shreesh.mishrs@gmail.com>
2026-09-20 01:16:07 +0530

    Configure automated

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include 'dbsetting.php';
 
 $social=mysqli_query($conx,'SELECT * FROM `register`');
 $sns= mysqli_fetch_assoc($social);
-/print_r($sns)
+print_r($sns)
 ?>
 <!DOCTYPE html>
 <html lang="en">
